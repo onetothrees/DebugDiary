2ff22f347fb977181ecbe12138ea0ae126e3c9bb
Era.Max <460837939@qq.com>
2026-04-28 10:30:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ README_TEMPLATE = """
   <a href="https://github.com/{user_name}/{repo_name}/actions"><strong>⚙️ 运行状态</strong></a>
 </p>
 
+# 将 README_TEMPLATE 中的图片部分改为以下代码
 <p align="center">
-  <img src="https://img.shields.io/github/last-commit/{user_name}/{repo_name}?style=flat-square&color=blue&label=最近更新">
-  <img src="https://img.shields.io/github/issues/{user_name}/{repo_name}?style=flat-square&color=orange&label=博文数量">
+  <img src="https://img.shields.io/github/last-commit/{user_name}/{repo_name}?style=flat-square&label=最近更新&color=blue">
+  <img src="https://img.shields.io/github/issues/{user_name}/{repo_name}?style=flat-square&label=博文数量&color=orange">
 </p>
 
 ---
